09f3aabda58e033da807f3edd4066f8b440db150
Nagib Mahfuz <120543584+nagibgithub@users.noreply.github.com>
2023-03-30 01:13:25 +0600

    change icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,9 @@ import './Header.css'
 const Header = () => {
     return (
         <nav>
-            <a href="home"><img src="/src/images/Logo.svg" alt="" /></a>
+            <a href="home"><img src="../../images/Logo.svg" alt="" /></a>
             <div>
-                {/* a*4[href=$]{$} */ }
+                {/* a*4[href=$]{$} */}
                 <a href="/order">Order</a>
                 <a href="/orderReview">Order Review</a>
                 <a href="/manageInventory">Manage Inventory</a>
